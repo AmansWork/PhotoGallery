8b8c5595497144f1526efba56662dc2e7e87f925
Aman Kesharwani <amankesharwani990@gmail.com>
2023-04-19 00:49:29 +0530

    without Jumbotron 1.0

--- src/components/navBar/Home.jsx
+++ src/components/navBar/Home.jsx
@@ -3,15 +3,6 @@ import HomeAboutCards from "../homeRelatedAbouts/HomeAboutCards";
 
 export default function Home() {
     return (<>
-
-        {/* <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Fluid jumbotron</h1>
-                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-                <h1 class="display-4">Fluid jumbotron</h1>
-                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-            </div>      
-        </div> */}
         <div>
             <GetImages />
             <br />
